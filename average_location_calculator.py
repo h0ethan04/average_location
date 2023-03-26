5ffba833e97e035018da581665821fdad98a3e8b
Ethan Ho <hoea2@uci.edu>
2023-03-26 00:03:44 -0700

    tracking main

---
+++ average_location_calculator.py
@@ -0,0 +1,10 @@
+import input_reading
+
+def main():
+    center = input_reading.read_central_location()
+    for address in input_reading.read_locations():
+        pass
+
+
+if __name__ == '__main__':
+    main()
